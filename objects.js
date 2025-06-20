951fcbe19b806a174f6863606b14b3ee5037cb98
Abinayask003 <abinayask.23eie@kongu.edu>
2025-06-20 08:57:17 +0530

    complete objsets

---
+++ objects.js
@@ -0,0 +1,41 @@
+// function add(a, b) {
+//   console.log(a + b);
+// }
+
+// let newObject = {
+//   name: "John Doe",
+//   age: 25,
+//   func: (a, b) => {
+//     console.log(a + b);
+//   },
+// };
+
+// console.log(newObject.name);
+// console.log(newObject.age);
+// newObject.func(2, 3);
+
+function getDetails() {
+  let person = {
+    name: "John Doe",
+    age: 25,
+    role: "developer",
+    company: "ABC Pvt Ltd",
+    isMarried: false,
+    salary: 2.5,
+  };
+  return person;
+}
+
+let person = {
+  name: "John Doe",
+  age: 25,
+};
+let newDetails = {
+  ...person,
+  role: "developer",
+  company: "ABC Pvt Ltd",
+  isMarried: false,
+  salary: 2.5,
+};
+
+console.log(newDetails);
